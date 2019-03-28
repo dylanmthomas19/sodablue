90fad55c41bfb0fb477b0df642c15d66373266af
Dylan-Desktop <dylanmthomas@live.com>
2019-03-28 17:40:10 -0600

    testing moving public in to client

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 5280;
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+};
+
+// Add routes, both API and view
+app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`Server now listening on PORT ${PORT}!`);
+});
